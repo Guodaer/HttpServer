4ae2011b8ced06040b1cc3ddddc0e38d81192ae1
guoda <1023251767@qq.com>
2017-03-17 14:43:36 +0800

    Initial Commit

---
+++ HttpServer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpServer
+//
+//  Created by X-Designer on 17/3/17.
+//  Copyright © 2017年 Guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HttpServer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HttpServer
+//
+//  Created by X-Designer on 17/3/17.
+//  Copyright © 2017年 Guoda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
